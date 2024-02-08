e99385dfbb541b9051bf598ed687902f11cfa5d6
karthik1220 <karthikgithub1102@gmail.com>
2024-02-08 23:18:47 +0530

    create user completed

--- src/main/java/com/kartheek/springsecurity/auth/service/UserServiceImpl.java
+++ src/main/java/com/kartheek/springsecurity/auth/service/UserServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl  implements UserService{
     @Autowired
     private ModelMapper modelMapper;
+    @Autowired
     private UserInfoRepository userInfoRepository;
     @Override
     public RegisterReqDTO createUser(RegisterReqDTO registerReqDTO) {
